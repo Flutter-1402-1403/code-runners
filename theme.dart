86bbb375901924c176edf26dcd5a20bfcb5346b1
LAST_V <78997013+Arshia-v@users.noreply.github.com>
2024-03-08 20:23:02 +0330

    Delete theme.dart

--- theme.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-class LightThemeColors {
-  static const primaryColor = Color(0xff217CF3);
-  static const secondaryColor = Color(0xff262A35);
-  static const primaryTextColor = Color(0xff262A35);
-  static const secondaryTextColor = Color(0xffB3B6BE);
-}
